9de2388805d39a2799b025462926931e4005aded
RPChinhara <rpchinhara@gmail.com>
2023-03-07 09:34:47 +0530

    finding the first word in a string literal

--- find_word/src/main.rs
+++ find_word/src/main.rs
@@ -1,10 +1,18 @@
 fn main() {
-    let mut s = String::from("hello");
+    let mut s = String::from("hello world");
+    let _word = first_word(&s[0..6]);
+    let _word = first_word(&s[..]);
     let _word = first_word(&s);
+
+    let my_string_literal = "hello world";
+    let _word = first_word(&my_string_literal[0..6]);
+    let _word = first_word(&my_string_literal[..]);
+    let _word = first_word(&my_string_literal);
+
     s.clear();
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
